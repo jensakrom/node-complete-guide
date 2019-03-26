801db930b7e101be03f76feeaffb751b0453a721
jensakrom <jensakrom@gmail.com>
2019-03-26 17:14:00 +0700

    expressjs

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" methode="POST"><input type="text" name= "title"><button type="submit"> Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello</h1>');
+});
+
+
+app.listen(3000);
